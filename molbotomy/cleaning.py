a58eca54ae1fdff53f922dcc51a001951c794f97
derekvantilborg <dwvantilborg@gmail.com>
2024-01-31 11:39:13 +0100

    moved constant variables to a separate file

--- molbotomy/cleaning.py
+++ molbotomy/cleaning.py
@@ -17,6 +17,8 @@ Jan 2024
 """
 from molbotomy.tools import canonicalize_smiles
 from molbotomy.utils import smiles_tokenizer
+from molbotomy.constants import SOLVENTS, NEUTRALIZATION_PATTERNS
+from rdkit.Chem.SaltRemover import SaltRemover
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from collections import Counter
@@ -179,7 +181,6 @@ def desalter(smiles, salt_smarts: str = "[Cl,Na,Mg,Ca,K,Br,Zn,Ag,Al,Li,I,O,N,H]"
     if '.' not in smiles:
         return smiles
 
-    from rdkit.Chem.SaltRemover import SaltRemover
     remover = SaltRemover(defnData=salt_smarts)
 
     new_smi = Chem.MolToSmiles(remover.StripMol(Chem.MolFromSmiles(smiles)))
@@ -208,15 +209,7 @@ def remove_common_solvents(smiles: str) -> str:
     if '.' not in smiles:
         return smiles
 
-    solvents = ['O=C(O)C(F)(F)F', 'O=C(O)C(=O)O', 'O=C(O)/C=C/C(=O)O', 'CS(=O)(=O)O', 'O=C(O)/C=C\\C(=O)O', 'CC(=O)O',
-                'O=S(=O)(O)O', 'O=CO', 'CCN(CC)CC', '[O-][Cl+3]([O-])([O-])[O-]', 'O=C(O)C(O)C(O)C(=O)O',
-                'Cc1ccc(S(=O)(=O)[O-])cc1', 'O=C([O-])C(F)(F)F', 'Cc1ccc(S(=O)(=O)O)cc1', 'O=C(O)CC(O)(CC(=O)O)C(=O)O',
-                'O=[N+]([O-])O', 'F[B-](F)(F)F', 'O=S(=O)([O-])C(F)(F)F', 'F[P-](F)(F)(F)(F)F', 'O=C(O)CCC(=O)O',
-                'O=P(O)(O)O', 'NCCO', 'CS(=O)(=O)[O-]', '[O-][Cl+3]([O-])([O-])O', 'COS(=O)(=O)[O-]', 'NC(CO)(CO)CO',
-                'CCO', 'CN(C)C=O', 'O=C(O)[C@H](O)[C@@H](O)C(=O)O', 'C1CCC(NC2CCCCC2)CC1', 'C', 'O=S(=O)([O-])O',
-                'CNC[C@H](O)[C@@H](O)[C@H](O)[C@H](O)CO', 'c1ccncc1']
-
-    for solv in solvents:
+    for solv in SOLVENTS:
         smiles = desalter(smiles, solv)
 
     return smiles
@@ -239,27 +232,7 @@ def unrepeat_smiles(smiles: str):
 
 def _initialise_neutralisation_reactions() -> list[(str, str)]:
     """ adapted from the rdkit contribution of Hans de Winter """
-    patts = (
-        # Imidazoles
-        ('[n+;H]', 'n'),
-        # Amines
-        ('[N+;!H0]', 'N'),
-        # Carboxylic acids and alcohols
-        ('[$([O-]);!$([O-][#7])]', 'O'),
-        # Thiols
-        ('[S-;X1]', 'S'),
-        # Sulfonamides
-        ('[$([N-;X2]S(=O)=O)]', 'N'),
-        # Enamines
-        ('[$([N-;X2][C,N]=C)]', 'N'),
-        # Tetrazoles
-        ('[n-]', '[nH]'),
-        # Sulfoxides
-        ('[$([S-]=O)]', 'S'),
-        # Amides
-        ('[$([N-]C=O)]', 'N'),
-    )
-    return [(Chem.MolFromSmarts(x), Chem.MolFromSmiles(y, False)) for x, y in patts]
+    return [(Chem.MolFromSmarts(x), Chem.MolFromSmiles(y, False)) for x, y in NEUTRALIZATION_PATTERNS]
 
 
 def sanitize_mol(smiles: str) -> str:
